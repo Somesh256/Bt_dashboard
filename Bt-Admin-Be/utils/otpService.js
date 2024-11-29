348bd5e505d3f8a7f95600c19b14f16500e81228
somesh <urwithsomesh@gmail.com>
2024-11-29 15:03:38 +0530

    update

--- Bt-Admin-Be/utils/otpService.js
+++ Bt-Admin-Be/utils/otpService.js
@@ -1,26 +1,26 @@
-// const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey('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');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
-// async function sendOTPEmail(email, otp) {
-//     try {
-//         const msg = {
-//             to: email,
-//             from: 'someshkumarmeena@outlook.com',
-//             subject: 'Email Verification OTP',
-//             text: `Your OTP for email verification is: ${otp}`,
-//             cc: 'ddmahadev@gmail.com',
-//             bcc: 'abc@gmail.com'
-//         };
-//         await sgMail.send(msg);
-//         return true
-//     } catch (error) {
-//         console.error('Error sending OTP email:', error);
-//         throw new Error('Failed to send OTP email');
-//     }
-// }
+async function sendOTPEmail(email, otp) {
+    try {
+        const msg = {
+            to: email,
+            from: 'someshkumarmeena@outlook.com',
+            subject: 'Email Verification OTP',
+            text: `Your OTP for email verification is: ${otp}`,
+            cc: 'ddmahadev@gmail.com',
+            bcc: 'abc@gmail.com'
+        };
+        await sgMail.send(msg);
+        return true
+    } catch (error) {
+        console.error('Error sending OTP email:', error);
+        throw new Error('Failed to send OTP email');
+    }
+}
 
-// function generateOTP() {
-//     return Math.floor(100000 + Math.random() * 900000).toString();
-// }
+function generateOTP() {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+}
 
-// module.exports = { sendOTPEmail, generateOTP };
+module.exports = { sendOTPEmail, generateOTP };
